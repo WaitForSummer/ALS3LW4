b5b445b6691777eb338ca10575c7048b36f817ca
Wait For-Summer <58130219+WaitForSummer@users.noreply.github.com>
2025-09-27 15:09:19 +0300

    updated MyMatrix.cs (added overriden operators)

--- LW4/LW4/MyMatrix.cs
+++ LW4/LW4/MyMatrix.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Serialization.Formatters;
 
 namespace LW4
 {
@@ -8,19 +9,6 @@ namespace LW4
         private int m;
         private int n;
         private double[,] matrix;
-
-        // GoRN
-        private void RandomEl (double min, double max) 
-        { 
-            Random random = new Random();
-            for (int i = 0; i < n; i++) 
-            {
-                for (int j = 0; j < n; j++) 
-                {
-                    matrix[i, j] = (random.NextDouble() + min) % max;
-                }
-            }
-        }
         
         // Constructor
         public MyMatrix(int m, int n)
@@ -41,6 +29,100 @@ namespace LW4
             // Entering max value
             Console.WriteLine("Enter max value for elements: ");
             double max = double.Parse(Console.ReadLine());
+
+            // filling elements w rand values
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    matrix[i, j] = (random.NextDouble() + min) % max;
+        }
+
+        // Overriden operators
+        // overrriden summary operator
+        public static MyMatrix operator +(MyMatrix a, MyMatrix b)
+        {
+            // initializing
+            MyMatrix newMatrix = new MyMatrix(a.m, a.n);
+
+            // summary 
+            for (int i = 0; i < a.m; i++)
+                for (int j = 0; j < a.n; j++)
+                    newMatrix.matrix[i, j] = a.matrix[i, j] + b.matrix[i, j];
+
+            // returning result
+            return newMatrix;
+        }
+
+        // overrriden difference operator
+        public static MyMatrix operator -(MyMatrix a, MyMatrix b)
+        {
+            // initializing
+            MyMatrix newMatrix = new MyMatrix(a.m, a.n);
+
+            // summary 
+            for (int i = 0; i < a.m; i++)
+                for (int j = 0; j < a.n; j++)
+                    newMatrix.matrix[i, j] = a.matrix[i, j] - b.matrix[i, j];
+
+            // returning result
+            return newMatrix;
+        }
+
+        // overriden multiplying by number (matrix from the left side)
+        public static MyMatrix operator*(MyMatrix a, double num)
+        {
+            // initializing
+            MyMatrix newMatrix = new MyMatrix(a.m, a.n);
+
+            // multiplying
+            for (int i = 0; i < a.m; i++)
+                for (int j = 0; j < a.n; j++)
+                    newMatrix.matrix[i, j] = num * a.matrix[i, j];
+
+            // returning result
+            return newMatrix;
+        }
+
+        // overriden multiplying by number (number from the left side)
+        public static MyMatrix operator *(double num, MyMatrix a)
+        {
+            MyMatrix newMatrix = a * num;
+
+            return newMatrix;
+        }
+
+        // overriden dividing by number (matrix from the left side)
+        public static MyMatrix operator /(MyMatrix a, double num)
+        {
+            // initializing
+            MyMatrix newMatrix = new MyMatrix(a.m, a.n);
+
+            // multiplying
+            for (int i = 0; i < a.m; i++)
+                for (int j = 0; j < a.n; j++)
+                    newMatrix.matrix[i, j] = num / a.matrix[i, j];
+
+            // returning result
+            return newMatrix;
+        }
+
+        // overriden dividing by number (number from the left side)
+        public static MyMatrix operator /(double num, MyMatrix a)
+        {
+            MyMatrix newMatrix = a / num;
+
+            return newMatrix;
+        }
+
+        // overriden multiplying among matrix
+        public static MyMatrix operator *(MyMatrix a, MyMatrix b)
+        {
+            if (a.n != b.m)
+            {
+                throw new ArgumentException("Wrong matrix to multiplying");
+            }
+
+
         }
     }
 }
