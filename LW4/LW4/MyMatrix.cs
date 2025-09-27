b725e7232dfd5d784159f863ac690e8840cca1e6
Wait For-Summer <58130219+WaitForSummer@users.noreply.github.com>
2025-09-28 00:05:21 +0300

    updated MyMatrix.cs (added new functions, overriden indexator, updated previous functions)

--- LW4/LW4/MyMatrix.cs
+++ LW4/LW4/MyMatrix.cs
@@ -6,7 +6,9 @@ namespace LW4
     public class MyMatrix
     {
         // fields
-        private int m;
+        // row
+        private int m; 
+        // column
         private int n;
         private double[,] matrix;
         
@@ -37,10 +39,33 @@ namespace LW4
                     matrix[i, j] = (random.NextDouble() + min) % max;
         }
 
+        // user indexator
+        public double this[int row, int col]
+        {
+            get 
+            {
+                if (row < 0 || row >= m || col < 0 || col >= n)
+                    throw new IndexOutOfRangeException("Oops! Index out of range");
+
+                return matrix[row, col];
+            }
+            set
+            {
+                if (row < 0 || row >= m || col < 0 || col >= n)
+                    throw new IndexOutOfRangeException("Oops! Index out of range");
+
+                matrix[row, col] = value;
+            }
+        }
+
         // Overriden operators
         // overrriden summary operator
         public static MyMatrix operator +(MyMatrix a, MyMatrix b)
         {
+            // checking
+            if (a.m != b.m || a.n != b.n)
+                throw new InvalidOperationException("You can only summarize matrices of the same size.");
+
             // initializing
             MyMatrix newMatrix = new MyMatrix(a.m, a.n);
 
@@ -56,6 +81,9 @@ namespace LW4
         // overrriden difference operator
         public static MyMatrix operator -(MyMatrix a, MyMatrix b)
         {
+            // checking
+            if (a.n != b.n || a.m != b.m) 
+                throw new InvalidOperationException("You can only substract mattrices of the same size.");
             // initializing
             MyMatrix newMatrix = new MyMatrix(a.m, a.n);
 
@@ -91,9 +119,12 @@ namespace LW4
             return newMatrix;
         }
 
-        // overriden dividing by number (matrix from the left side)
+        // overriden dividing by number
         public static MyMatrix operator /(MyMatrix a, double num)
         {
+            // checking
+            if (num == 0)
+                throw new DivideByZeroException("Sudenly, you can't divide by zero(((");
             // initializing
             MyMatrix newMatrix = new MyMatrix(a.m, a.n);
 
@@ -106,23 +137,29 @@ namespace LW4
             return newMatrix;
         }
 
-        // overriden dividing by number (number from the left side)
-        public static MyMatrix operator /(double num, MyMatrix a)
-        {
-            MyMatrix newMatrix = a / num;
-
-            return newMatrix;
-        }
-
         // overriden multiplying among matrix
         public static MyMatrix operator *(MyMatrix a, MyMatrix b)
         {
+            // checking
             if (a.n != b.m)
+                throw new InvalidOperationException("Wrong matrix to multiplying");
+
+            // initializing
+            MyMatrix newMatrix = new MyMatrix(a.m, a.n);
+
+            // multiplying
+            for (int i = 0; i < a.m; ++i)
             {
-                throw new ArgumentException("Wrong matrix to multiplying");
+                for (int j = 0; j < b.n; ++j)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < a.n; ++k)
+                        sum += a.matrix[i, k] * b.matrix[k, j];
+
+                    newMatrix.matrix[i, j] = sum;
+                }
             }
-
-
+            return newMatrix;
         }
     }
 }
