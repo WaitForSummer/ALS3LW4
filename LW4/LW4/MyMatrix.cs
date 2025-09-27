b22c8bdd656d24e516400890cb15b8d693675fa7
Wait For-Summer <58130219+WaitForSummer@users.noreply.github.com>
2025-09-27 10:42:09 +0300

    added MyMatrix.cs

---
+++ LW4/LW4/MyMatrix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LW4
+{
+    public class MyMatrix
+    {
+        // fields
+        private int m;
+        private int n;
+        private double[,] matrix;
+
+        // Constructor
+        public MyMatrix(int m, int n)
+        {
+            // Check for correct params
+            if (m <= 0 || n <= 0)
+                throw new ArgumentException("Count of n or/and m must be >0");
+
+            // initializing
+            this.m = m;
+            this.n = n;
+            matrix = new double[m, n];
+
+            // Entering min value
+            Console.WriteLine("Enter min value for elements: ");
+            double min = double.Parse(Console.ReadLine());
+
+            // Entering max value
+            Console.WriteLine("Enter max value for elements: ");
+            double max = double.Parse(Console.ReadLine());
+        }
+
+    }
+}
