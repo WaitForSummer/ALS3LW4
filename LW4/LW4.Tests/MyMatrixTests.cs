986f30035bca2ec07cec62e9bc8d0b1ee715d967
Wait For-Summer <58130219+WaitForSummer@users.noreply.github.com>
2025-09-28 20:49:34 +0300

    added MyMatrix.cs

---
+++ LW4/LW4.Tests/MyMatrixTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace LW4.Tests
+{
+    [TestClass]
+    public sealed class MyMatrixTests
+    {
+        // min and max values
+        private double minTest = 1.0;
+        private double maxTest = 10.0;
+
+        // method
+        private MyMatrix CreatetestMatrix(int rows, int columns)
+        {
+            var matrix = new MyMatrix(rows, columns, minTest, maxTest);
+        }
+
+        // Testing constructor
+        [TestMethod]
+        public void constructorTesting()
+        {
+            MyMatrix testMatrix = new MyMatrix(3, 4);
+        }
+    }
+}
