ea2404d2234588da1061378e499f06c82281947f
Wait For-Summer <58130219+WaitForSummer@users.noreply.github.com>
2025-09-29 13:43:14 +0300

    updated Program.cs

--- LW4/LW4.Console/Program.cs
+++ LW4/LW4.Console/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices.Marshalling;
 using LW4;
 
 namespace LW4.Console
@@ -60,9 +62,65 @@ namespace LW4.Console
             resM.Print();
         }
 
+        public static void Task2()
+        {
+            // Demonstrating work for the second task
+            // initialize variables
+            // array of cars
+            Car[] cars =
+            {
+                new Car("Toyota Camry", 2020, 210),
+                new Car("BMW M5", 2018, 250),
+                new Car("Audi A5", 2022, 240),
+                new Car("Honda Civic", 2019, 200),
+                new Car("Mercedec G-Class", 2021, 230)
+            };
+
+            System.Console.WriteLine("Array of cars: ");
+            for (int i = 0; i < cars.Length; i++) 
+            {
+                System.Console.WriteLine(cars[i].ToString());
+            }
+
+            // Demonstrating sort by name
+            System.Console.WriteLine("\n=== Sort by Name ===");
+            Array.Sort(cars, new CarComparer(CarComparer.SortBy.Name));
+            for (int i = 0; i < cars.Length; i++) 
+            {
+                System.Console.WriteLine(cars[i].ToString());
+            }
+
+            // Demonstrating sort by year
+            System.Console.WriteLine("\n=== Sort by Year ===");
+            Array.Sort(cars, new CarComparer(CarComparer.SortBy.Year));
+            for (int i = 0;i < cars.Length; i++)
+            {
+                System.Console.WriteLine(cars[i].ToString());
+            }
+
+            // Demonstrating sort by speed
+            System.Console.WriteLine("\n=== Sort by Speed ===");
+            Array.Sort(cars, new CarComparer(CarComparer.SortBy.Speed));
+            for (int i = 0; i < cars.Length; i++) 
+            {
+                System.Console.WriteLine(cars[i].ToString());
+            }
+
+            // Demonstrating sort by year reverse
+            System.Console.WriteLine("\n=== Sort by Year reverse ===");
+            Array.Sort(cars, new CarComparer(CarComparer.SortBy.Year));
+            Array.Reverse(cars);
+            for (int i = 0; i <= cars.Length; i++) 
+            {
+                System.Console.WriteLine(cars[i].ToString());
+            }
+        }
+
         public static void Main(string[] args)
         {
             Task1();
+
+            Task2();
         }
     }
 }
